f43b72ece3c52db4cc5119812ee66f51de11c7a9
Terry <t3rrygong@gmail.com>
2020-05-11 15:06:26 -0600

    every new notes will have a unqiue id. finished apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,4 +16,13 @@ module.exports = function (app) {
             res.send(200);
         });
     });
+    app.delete("/api/notes/:id", function (req, res) {
+        const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+        const noteToDelete = notes.find(notes => notes.id === req.params.id);
+        notes.splice(notes.indexOf(noteToDelete), 1);
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.send(200);
+        });
+    });
 };
